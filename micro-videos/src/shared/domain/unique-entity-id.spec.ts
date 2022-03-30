a5b38613dbdf0ae5aa52856a8506fd8d4d540424
Joao Sousa <joao.rsousa23@gmail.com>
2022-03-30 21:03:18 +1030

    chore: add remote container

--- micro-videos/src/shared/domain/unique-entity-id.spec.ts
+++ micro-videos/src/shared/domain/unique-entity-id.spec.ts
@@ -2,15 +2,19 @@ import { v4, validate } from 'uuid';
 import InvalidUuidError from '../../category/domain/errors/invalid-uuid.error';
 import UniqueEntityId from './unique-entity-id';
 
+function spyValidationMethod() {
+  return jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+}
+
 describe('UniqueEntityId Unit Tests                                                                                              ', () => {
   it('should ', () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+    const validateSpy = spyValidationMethod();
     expect(() => new UniqueEntityId('fake id')).toThrow(new InvalidUuidError());
     expect(validateSpy).toHaveBeenCalled();
   });
 
   it('should accept a uuid passed in constructor', () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+    const validateSpy = spyValidationMethod();
 
     const uuid = v4();
     const vo = new UniqueEntityId(uuid);
@@ -19,7 +23,7 @@ describe('UniqueEntityId Unit Tests                                             
   });
 
   it('should accept an empty id passed in constructor', () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+    const validateSpy = spyValidationMethod();
 
     const vo = new UniqueEntityId();
     expect(validate(vo.id)).toBeTruthy();
